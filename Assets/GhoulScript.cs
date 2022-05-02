5d9d95d052d58d980c9a0daee070394c72bba099
GamerGlasses <69730370+GamerGlasses@users.noreply.github.com>
2022-05-02 03:39:25 +0100

    ghoul changes

--- Assets/GhoulScript.cs
+++ Assets/GhoulScript.cs
@@ -24,60 +24,17 @@ public class GhoulScript : MonoBehaviour
     void Start()
     {
         target = GameObject.Find("Player(Clone)").transform;
-
-        //agent.SetDestination(new Vector3(target.position.x, 0, target.position.z));
-        destination =FindPos(target.position);
-
         agent.SetDestination(destination);
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(target.position);
         if (Vector3.Distance(target.position, transform.position) < 10f && canSwoop)
         {
-            //agent.SetDestination(new Vector3(target.position.x, 0, target.position.z));
-            destination = FindPos(target.position);
-            agent.SetDestination(destination);
             StartCoroutine("swoop");
         }
-        if (swooping == false)
-        {
-            if (Vector3.Distance(destination,transform.position)<0.5f)
-            {
-
-                destination = FindPos(new Vector3(Random.Range(-4, 4) + target.position.x, target.position.y, Random.Range(-4, 4) + target.position.z));
-               // destination = new Vector3(Random.Range(-4, 4)+ target.position.x, 0, Random.Range(-4, 4)+target.position.z);
-
-                agent.SetDestination(destination);
-            }
-            //if (Vector3.Distance(target.position, transform.position)d > 10f)
-            //{
-
-            //    agent.SetDestination(target.position + new Vector3(Random.Range(-4, 4), 0, Random.Range(-4, 4)));
-            //    destination = target.position;
-            //}
-        }
-    }
-    Vector3 FindPos(Vector3 pos)
-    {
-        Vector3 ItemPosition = new Vector3();
-        bool ready = false;
-        int x = 0;
-        while (!ready)
-        {
-            Vector3 topPosition = (Vector3)Random.insideUnitCircle*3 + pos;
-            topPosition = new Vector3(topPosition.x, 10, topPosition.z);
-            Vector3 placePosition = UtilityManager.Instance.ShootRayCastDown(topPosition);
-            if (placePosition != Vector3.zero)
-            {
-                ready = true;
-                ItemPosition = placePosition;
-            }
-            x++;
-            if (x > 50) return Vector3.zero;
-        }
-        return ItemPosition;
     }
 
     IEnumerator swoop()
